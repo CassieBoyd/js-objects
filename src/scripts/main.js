95da8917c8d311daf19ce36a31a719b5a41155be
Cassie Boyd <cassandra.bacon@gmail.com>
2019-10-09 14:42:34 -0500

    Practiced objects vs arrays

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,34 @@
-console.log("Learning about objects!");
+console.log("We're learning about objects!");
+
+const car = {
+    make: "Ford",
+    model: "Mustang",
+    year: 2015,
+    color: "red"
+};
+
+const animals = ["Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", "Sharky"];
+
+const cassie = {
+    food: "Salad",
+    color: "Purple"
+};
+
+const ken = {
+    food: "Pizza",
+    color: "Grey"
+};
+
+const peach = {
+    food: "Tuna",
+    color: "Grey and white"
+};
+
+const ginger = {
+    food: "Peanut butter",
+    color: "Gold"
+}
+
+const familyMembers = [ken, cassie, peach, ginger];
+
+console.log("family members", familyMembers)
